2cee99071c60aeecdd395e6e0a381e7d9fedb6fe
Chris Morrison <58083681+chrisnmorrison@users.noreply.github.com>
2021-10-05 17:54:32 -0400

    fix icons

--- src/components/common/Categories.js
+++ src/components/common/Categories.js
@@ -17,39 +17,39 @@ const Categories = () => {
       <div className="category-1-3">
         <div className="list_item">
           <a href="https://blog.mesmerdesign.ca/tag/advice/" className="categories_list_link">
-            <span className="category-background cat1"><img src="/images/icons/categories/wine.svg" alt="Wine icon" />Advice</span>
+            <span className="category-background cat1"><img className="category-icon" src="/images/icons/categories/wine.svg" alt="Wine icon" />Advice</span>
           </a>
         </div>
       </div>
       <div className="category-1-3">
         <div className="list_item">
           <a href="https://blog.mesmerdesign.ca/tag/cyberpsychology/" className="categories_list_link">
-            <span className="category-background cat2"><img src="/images/icons/categories/wine.svg" alt="Wine icon"/>Cyberpsychology</span> </a>
+            <span className="category-background cat2"><img className="category-icon" src="/images/icons/categories/wine.svg" alt="Wine icon"/>Cyberpsychology</span> </a>
         </div>
       </div>
       <div className="category-1-3">
         <div className="list_item">
           <a href="https://blog.mesmerdesign.ca/tag/innovation/" className="categories_list_link">
-            <span className="category-background cat3"><img src="/images/icons/categories/wine.svg" alt="Wine icon"/>Innovation</span> </a>
+            <span className="category-background cat3"><img className="category-icon" src="/images/icons/categories/wine.svg" alt="Wine icon"/>Innovation</span> </a>
         </div>
       </div>
       <div className="category-1-3">
         <div className="list_item">
           <a href="https://blog.mesmerdesign.ca/tag/inspiration/" className="categories_list_link">
-            <span className="category-background cat4"><img src="/images/icons/categories/wine.svg" alt="Wine icon"/>Inspiration</span> </a>
+            <span className="category-background cat4"><img className="category-icon" src="/images/icons/categories/wine.svg" alt="Wine icon"/>Inspiration</span> </a>
         </div>
       </div>
       <div className="category-1-3">
         <div className="list_item">
           <a href="https://blog.mesmerdesign.ca/tag/tools/" className="categories_list_link">
-            <span className="category-background cat5"><img src="/images/icons/categories/wine.svg" alt="Wine icon"/>Tools</span>
+            <span className="category-background cat5"><img className="category-icon" src="/images/icons/categories/wine.svg" alt="Wine icon"/>Tools</span>
           </a>
         </div>
       </div>
       <div className="category-1-3">
         <div className="list_item">
           <a href="https://blog.mesmerdesign.ca/tag/tutorials/" className="categories_list_link">
-            <span className="category-background cat6"><img src="/images/icons/categories/wine.svg" alt="Wine icon"/>Tutorials</span> </a>
+            <span className="category-background cat6"><img className="category-icon" src="/images/icons/categories/wine.svg" alt="Wine icon"/>Tutorials</span> </a>
         </div>
       </div>
     </div>
