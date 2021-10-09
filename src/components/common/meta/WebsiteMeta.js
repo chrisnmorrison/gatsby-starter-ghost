a4448ebc8f1061af914f7c6f615a73aafc8b6ce5
Chris Morrison <58083681+chrisnmorrison@users.noreply.github.com>
2021-10-09 09:23:15 -0400

    Update WebsiteMeta.js

--- src/components/common/meta/WebsiteMeta.js
+++ src/components/common/meta/WebsiteMeta.js
@@ -64,6 +64,15 @@ const WebsiteMeta = ({ data, settings, canonical, title, description, image, typ
                 {settings.twitter && <meta name="twitter:site" content={`https://twitter.com/${settings.twitter.replace(/^@/, ``)}/`} />}
                 {settings.twitter && <meta name="twitter:creator" content={settings.twitter} />}
                 <script type="application/ld+json">{JSON.stringify(jsonLd, undefined, 4)}</script>
+
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-3P41JGRRHM"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-3P41JGRRHM');
+                </script>
             </Helmet>
             <ImageMeta image={shareImage} />
         </>
